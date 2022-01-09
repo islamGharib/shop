63b8082f2c579b9f98d630bb026498d19d237afc
islamgharib <islamahmed1992@yahoo.com>
2022-01-09 18:01:24 +0200

    creating on_boarding model

---
+++ lib/modules/on_boarding/on_boarding_screen.dart
@@ -0,0 +1,12 @@
+// model
+class BoardingModel{
+  final String image;
+  final String title;
+  final String body;
+
+  BoardingModel({
+    required this.title,
+    required this.image,
+    required this.body,
+  });
+}

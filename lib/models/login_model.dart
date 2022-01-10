33881a9ece940bf7e9df7ea731d68b2bdd5974b0
islamGharib <islamahmed1992@yahoo.com>
2022-01-10 09:45:18 +0200

    creating login model

---
+++ lib/models/login_model.dart
@@ -0,0 +1,44 @@
+class ShopLoginModel{
+  bool status;
+  String message;
+  UserData? data;
+            // named constructor
+  ShopLoginModel.fromJson(Map<String, dynamic> json)
+    : status = json['status'],
+      message = json['message'],
+      data = json['data'] != null ? UserData.fromJson(json['data']): null;
+}
+
+class UserData {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String image;
+  int points;
+  int credit;
+  String token;
+            // named constructor
+  UserData.fromJson(Map<String, dynamic> json)
+   : id = json['id'],
+    name = json['name'],
+    email = json['email'],
+    phone = json['phone'],
+    image = json['image'],
+    points = json['points'],
+    credit = json['credit'],
+    token = json['token'];
+
+            // normal constructor
+//   UserData(
+//     this.id,
+//     this.name,
+//     this.email,
+//     this.phone,
+//     this.image,
+//     this.points,
+//     this.credit,
+//     this.token
+// );
+
+}

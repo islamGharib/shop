1245442e40d871cab29ac203ffe4f9bcd77cdf9f
islamGharib <islamahmed1992@yahoo.com>
2022-01-10 09:58:49 +0200

    creating login cubit and states

---
+++ lib/modules/shop_login/cubit/states.dart
@@ -0,0 +1,14 @@
+import 'package:shop_app/models/login_model.dart';
+
+abstract class ShopLoginState{}
+class ShopLogininitialState extends ShopLoginState{}
+class ShopLoginLoadingState extends ShopLoginState{}
+class ShopLoginSuccessState extends ShopLoginState{
+  final ShopLoginModel loginModel;
+  ShopLoginSuccessState(this.loginModel);
+}
+class ShopLoginErrorState extends ShopLoginState{
+  final String error;
+  ShopLoginErrorState(this.error);
+}
+class ShopLoginChangePasswordVisibilityState extends ShopLoginState{}

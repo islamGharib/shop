71bf0ea67e04edc68e5904aab3cfd644dc5153d8
islamgharib <islamahmed1992@yahoo.com>
2022-01-11 11:58:23 +0200

    caching user token

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,40 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:shop_app/modules/on_boarding/on_boarding_screen.dart';
 import 'package:shop_app/shared/bloc_provider.dart';
+import 'package:shop_app/shared/component/constants.dart';
 import 'package:shop_app/shared/network/local/cach_helper.dart';
 import 'package:shop_app/shared/network/remote/dio_helper.dart';
 import 'package:shop_app/shared/styles/themes.dart';
+
+import 'layout/shop_layout/shop_layout_Screen.dart';
+import 'modules/shop_login/shop_login_screen.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = MyBlocObserver();
   await CachHelper.init();
   DioHelper.init();
-  runApp(MyApp());
+  Widget widget;
+  dynamic onBoarding = CachHelper.getData(key: 'onBoarding');
+
+  if(onBoarding != null){
+    if(token != null) widget = ShopLayoutScreen();
+    else widget = ShopLoginScreen();
+  }else widget = OnBoardingScreen();
+
+  runApp(MyApp(widget));
 }
 
 class MyApp extends StatelessWidget {
+  Widget widget;
+  MyApp(this.widget);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
-      home: OnBoardingScreen(),
+      home: widget,
     );
   }
 }

6e47247fd48b700e2bd1a2cea2be37d08d8a9248
islamgharib <islamahmed1992@yahoo.com>
2022-01-11 12:24:38 +0200

    finishing shop_layout screen

--- lib/layout/shop_layout/shop_layout_Screen.dart
+++ lib/layout/shop_layout/shop_layout_Screen.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/layout/cubit/cubit.dart';
+import 'package:shop_app/layout/cubit/states.dart';
 
 class ShopLayoutScreen  extends StatelessWidget {
   const ShopLayoutScreen ({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(),
+    return BlocProvider(
+      create: (BuildContext context) => ShopCubit()..getHomeData()..getCategoryData(),
+      child: BlocConsumer<ShopCubit,ShopStates>(
+        listener: (context, state){},
+        builder: (context, state){
+          ShopCubit shopCubit = ShopCubit.get(context);
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(
+                shopCubit.titles[shopCubit.currentIndex],
+              ),
+              actions: [
+                IconButton(
+                  onPressed: (){},
+                  icon: Icon(
+                      Icons.search
+                  ),
+                ),
+              ],
+            ),
+            body: shopCubit.screens[shopCubit.currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: shopCubit.currentIndex,
+              onTap: (index){
+                shopCubit.changeShopBottomNavigationIndex(index);
+              },
+              items: shopCubit.bottomNavItems,
+            ),
+          );
+        },
+      ),
     );
   }
 }

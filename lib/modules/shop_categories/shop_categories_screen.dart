126f0f21ba4cc286737bef7d68d8e54376f9be9a
islamGharib <islamahmed1992@yahoo.com>
2022-01-18 15:05:03 +0200

    creating Categories screen

--- lib/modules/shop_categories/shop_categories_screen.dart
+++ lib/modules/shop_categories/shop_categories_screen.dart
@@ -1,15 +1,24 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shop_app/layout/cubit/cubit.dart';
+import 'package:shop_app/layout/cubit/states.dart';
 
 class ShopCategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Categories Screen',
-        ),
-      ),
+    return BlocConsumer<ShopCubit, ShopStates>(
+      listener: (context, state){},
+      builder: (context, state){
+        ShopCubit shopCubit = ShopCubit.get(context);
+        return ConditionalBuilder(
+          condition: shopCubit.categoriesModel != null && shopCubit.categoriesModel != null,
+          builder: (context) => CategoryBuilderItem(shopCubit.categoriesModel),
+          fallback: (context) => Center(child: CircularProgressIndicator(),),
+        );
+      },
+
     );
   }
 }

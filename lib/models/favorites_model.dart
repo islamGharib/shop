6b16c68965d791f6d3142d80e47d77dcc388814c
islamGharib <islamahmed1992@yahoo.com>
2022-01-24 12:07:13 +0200

    create Favorites Model

---
+++ lib/models/favorites_model.dart
@@ -0,0 +1,46 @@
+class FavoritesModel{
+  bool status ;
+  FavoriteDataModel favDataModel;
+  FavoritesModel.fromJson(Map<String, dynamic> json)
+      : status = json['status'],
+        favDataModel = FavoriteDataModel.fromJson(json['data']);
+}
+
+class FavoriteDataModel{
+  int currentPage;
+  List<FavoriteData> favDataList = [];
+  int total;
+  FavoriteDataModel.fromJson(Map<String, dynamic> json)
+      :  currentPage = json['current_page'],
+         favDataList = getFavorites(json),
+         total = json['total'];
+
+  static List<FavoriteData> getFavorites(Map<String,dynamic> json){
+    List<FavoriteData> favoriteList = [];
+    var favoriteData = json['data'];
+    for (var i in favoriteData) {
+      FavoriteData favorite = FavoriteData.fromJson(i);
+      favoriteList.add(favorite);
+    }
+    return favoriteList;
+  }
+
+}
+
+
+class FavoriteData{
+  int id;
+  String name;
+  String image;
+  dynamic price;
+  dynamic oldPrice;
+  dynamic discount;
+
+  FavoriteData.fromJson(Map<String,dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        image = json['image'],
+        price = json['price'],
+        oldPrice = json['old_price'],
+        discount = json['discount'];
+}

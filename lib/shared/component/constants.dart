dcfe50cf3f008dcb2dcf2427f4aee8002b05ece7
islamGharib <islamahmed1992@yahoo.com>
2022-01-25 15:32:29 +0200

    adding signOut function

--- lib/shared/component/constants.dart
+++ lib/shared/component/constants.dart
@@ -1,3 +1,13 @@
+import 'package:shop_app/modules/shop_login/shop_login_screen.dart';
+import 'package:shop_app/shared/component/components.dart';
 import 'package:shop_app/shared/network/local/cach_helper.dart';
 
 dynamic token = CachHelper.getData(key: 'token');
+
+void signOut(context){
+  CachHelper.clearData(key: 'token').then((value) {
+    if(value){
+      navigateAndFinish(context, ShopLoginScreen());
+    }
+  });
+}

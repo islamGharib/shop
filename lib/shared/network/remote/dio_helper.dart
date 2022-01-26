649d60990b52cc8662a863f78b6aa69247d8bcd1
islamGharib <islamahmed1992@yahoo.com>
2022-01-26 13:22:42 +0200

    adding putData function into DioHelper class for updating user profile

--- lib/shared/network/remote/dio_helper.dart
+++ lib/shared/network/remote/dio_helper.dart
@@ -52,6 +52,27 @@ class DioHelper{
     );
   }
 
+  static Future<Response> putData(
+      {
+        required String url,
+        Map<String, dynamic>? query,
+        required Map<String, dynamic> data,
+        String lang = 'ar',
+        String? token,
+      }
+      ) async{
+    dio!.options.headers = {
+      'lang': lang,
+      'Authorization':token,
+      'Content-Type': 'application/json',
+    };
+    return dio!.put(
+      url,
+      queryParameters: query,
+      data: data,
+    );
+  }
+
 }
 
 

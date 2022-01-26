3a986b651f871627b52ba70802f087280f790904
islamGharib <islamahmed1992@yahoo.com>
2022-01-26 14:50:31 +0200

    handling update user data

--- lib/modules/shop_settings/shop_settings_screen.dart
+++ lib/modules/shop_settings/shop_settings_screen.dart
@@ -14,6 +14,7 @@ class ShopSettingsScreen extends StatelessWidget {
     var emailController = TextEditingController();
     var nameController = TextEditingController();
     var phoneController = TextEditingController();
+    final formKey = GlobalKey<FormState>();
 
     return BlocConsumer<ShopCubit, ShopStates>(
         listener: (context, state){},
@@ -25,57 +26,76 @@ class ShopSettingsScreen extends StatelessWidget {
 
           return ConditionalBuilder(
             condition: ShopCubit.get(context).userDataModel != null,
-            builder: (context) => Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    defaultTextFormField(
-                      controller: nameController,
-                      type: TextInputType.text,
-                      validate:(value){
-                        if (value == null || value.isEmpty){
-                        return 'User name must not to be empty';
-                        }
-                        return null;
-                      },
-                      label: 'Name',
-                      prefix: Icons.person,
-                    ),
-                    SizedBox(height: 20.0,),
-                    defaultTextFormField(
-                      controller: emailController,
-                      type: TextInputType.emailAddress,
-                      validate:(value){
-                        if (value == null || value.isEmpty){
-                          return 'Email Address must not to be empty';
-                        }
-                        return null;
-                      },
-                      label: 'Email Address',
-                      prefix: Icons.email,
-                    ),
-                    SizedBox(height: 20.0,),
-                    defaultTextFormField(
-                      controller: phoneController,
-                      type: TextInputType.phone,
-                      validate:(value){
-                        if (value == null || value.isEmpty){
-                          return 'Phone Number must not to be empty';
-                        }
-                        return null;
-                      },
-                      label: 'Phone',
-                      prefix: Icons.phone,
-                    ),
-                    SizedBox(height: 20.0,),
-                    defaultButton(
-                        buttonPressed: (){
-                          signOut(context);
-                        },
-                        text: 'LOGOUT',
-                    ),
-                  ]
+            builder: (context) => SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if(state is ShopUpdateUserDataLoadingState)
+                        LinearProgressIndicator(),
+                        SizedBox(height: 20.0,),
+                        defaultTextFormField(
+                          controller: nameController,
+                          type: TextInputType.text,
+                          validate:(value){
+                            if (value == null || value.isEmpty){
+                            return 'User name must not to be empty';
+                            }
+                            return null;
+                          },
+                          label: 'Name',
+                          prefix: Icons.person,
+                        ),
+                        SizedBox(height: 20.0,),
+                        defaultTextFormField(
+                          controller: emailController,
+                          type: TextInputType.emailAddress,
+                          validate:(value){
+                            if (value == null || value.isEmpty){
+                              return 'Email Address must not to be empty';
+                            }
+                            return null;
+                          },
+                          label: 'Email Address',
+                          prefix: Icons.email,
+                        ),
+                        SizedBox(height: 20.0,),
+                        defaultTextFormField(
+                          controller: phoneController,
+                          type: TextInputType.phone,
+                          validate:(value){
+                            if (value == null || value.isEmpty){
+                              return 'Phone Number must not to be empty';
+                            }
+                            return null;
+                          },
+                          label: 'Phone',
+                          prefix: Icons.phone,
+                        ),
+                        SizedBox(height: 20.0,),
+                        defaultButton(
+                          buttonPressed: (){
+                            ShopCubit.get(context).updateUserData(
+                                name: nameController.text,
+                                email: emailController.text,
+                                phone: phoneController.text
+                            );
+                          },
+                          text: 'UPDATE',
+                        ),
+                        SizedBox(height: 20.0,),
+                        defaultButton(
+                            buttonPressed: (){
+                              signOut(context);
+                            },
+                            text: 'LOGOUT',
+                        ),
+                      ]
+                  ),
+                ),
               ),
             ),
             fallback: (context) => Center(child: CircularProgressIndicator(),),

be01ec8f7b75c853a099aabd31e8de505974b2a8
islamgharib <islamahmed1992@yahoo.com>
2022-01-27 16:48:30 +0200

    adding search states

---
+++ lib/modules/search/cubit/states.dart
@@ -0,0 +1,5 @@
+abstract class SearchStates{}
+class SearchProductDataInitialState extends SearchStates{}
+class SearchProductDataLoadingState extends SearchStates{}
+class SearchProductDataSuccessState extends SearchStates{}
+class SearchProductDataErrorState extends SearchStates{}
